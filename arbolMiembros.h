9b62029861d04ff1773a5d9ab19a4c13b7d1ad33
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2023-10-27 20:45:28 -0300

    Version 2
    Agregue funciones de arboles

--- arbolMiembros.h
+++ arbolMiembros.h
@@ -2,4 +2,48 @@
 #define ARBOLMIEMBROS_H_INCLUDED
 #include "biblioteca.h"
 
+// Estrutura Arbol
+
+typedef struct
+{
+
+    stMiembro dato;
+    struct nodoArbol *der;
+    struct nodoArbol *izq;
+
+} nodoArbol;
+
+// Estrutura Persona
+
+stPersona crearUnaPersona();
+
+void mostrarUnaPersona(stPersona aux);
+
+void mostrarUnMiembro(stMiembro aux)
+
+
+// Estrutura Miembro
+
+stMiembro crearUnMiembro();
+
+
+//Estrutura Arbol
+
+nodoArbol *inicarbol();
+
+nodoArbol *crearNodoArbol(stMiembro miembro);
+
+nodoArbol *buscarNodo(nodoArbol *arbol, int idMiembro);
+
+nodoArbol *insertarNodo(nodoArbol *raiz, nodoArbol *NN);
+
+void preorden(nodoArbol *raiz);
+
+void inorder(nodoArbol *raiz);
+
+void postorder(nodoArbol *raiz);
+
+nodoArbol *borrarNodo(nodoArbol *raiz, int idMiembro);
+
+
 #endif // ARBOLMIEMBROS_H_INCLUDED
